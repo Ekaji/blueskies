156df47b77b4855496d66ee2e1ef249018bb1769
ekaji <ekajionah@gmail.com>
2020-02-28 06:38:27 +0100

    added openweather  api

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      curWeather: []
+    }
+  }
+
+  componentDidMount(){
+    //use template string to get city
+    fetch('http://api.openweathermap.org/data/2.5/forecast?q=Abuja&appid=c9864ea908339f90d0b1ad79c454cf80')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render(){
+    return (
+      <div className="App">
+        <h1>Simple Weather App</h1>
+          <div>
+           <input type="search" />
+           <button>Search</button>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
-
 
