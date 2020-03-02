36725b804a45c448c8ce2eb4dd000dd126704183
ekaji <ekajionah@gmail.com>
2020-03-02 14:00:00 +0100

    secured api key

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './card_list_styles/gridStyle.css'
 import {Tempcomp} from './card_components/TempComponent'
+import apiConfig from './apiKey'
 import './App.css';
 
 class App extends Component{
@@ -8,19 +9,20 @@ class App extends Component{
     super();
     this.state = {
        dailyData: [],
-       City: ''
+       currCity: ''
     }
 }
    
 
 handleInput = (e) => {
-  this.setState({City: e.target.value})
+  this.setState({currCity: e.target.value})
   }
 
  handleSubmit = (e) => {
-   e.preventDefault()
-console.log(this.state.City)
-  fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.state.City}&appid=c9864ea908339f90d0b1ad79c454cf80`)
+   //prevents the form from submitting
+   e.preventDefault();
+   //used template literals to pass dynamic arguements to the api
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${this.state.currCity}&appid=${apiConfig.openWeatherKey}`)
   .then(res => res.json())
   .then(data => { const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
   this.setState({
@@ -35,11 +37,11 @@ console.log(this.state.City)
     return (
       <div className="App">
         <h1>Simple Weather App</h1>
-    <h2>city:{this.state.City}</h2>
+    <h2>city:{this.state.currCity}</h2>
           <div>
 
             <form onSubmit={this.handleSubmit}>
-              <input onChange={this.handleInput} value={this.state.City} /> 
+              <input onChange={this.handleInput} value={this.state.currCity} /> 
               <button type="submit" >submit</button>
             </form>
 
@@ -53,37 +55,7 @@ console.log(this.state.City)
   }
 }
 
-// class App extends Component {
-//   state = {
-//     inputText: '',
-//     mouseX: 0,
-//     mouseY: 0    
-//   }
-//   handleInput = (e) => {
-//     this.setState({inputText: e.target.value});
-//   }
-//   handleSubmit = () => {
-//     alert(`Quoteth Shakespeare, "You cad! ${this.state.inputText}"`);
-//   }
- 
-//   render() {
-//     return (
-//       <div>
 
-//         {/* <input
-//           onChange={this.handleInput}  
-//           value={this.state.inputText}
-//         /> */}
-
-//         <form onSubmit={console.log(this.state.inputText) }>  {/* 📥 onsubmit */}
-//           <input onChange={this.handleInput.bind(this)} value={this.state.inputText}/>
-//           <button type="submit">submit dis</button>
-//         </form>
-
-//       </div>
-//     )
-//   }
-// }
 
 export default App;
 
