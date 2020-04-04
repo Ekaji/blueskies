0a9c3a2d155db5c8e44f4fd6a02df1f3ab3e857f
ekaji <ekajionah@gmail.com>
2020-04-04 21:52:03 +0100

    Updates

---
+++ precache-manifest.ffa6e3d210b1347fe152e441dbc3b807.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9772d302211b5b2b53e801618807f890",
+    "url": "/blueskies/index.html"
+  },
+  {
+    "revision": "eb1b552c266845876c1a",
+    "url": "/blueskies/static/css/main.8ec9665a.chunk.css"
+  },
+  {
+    "revision": "f040eef0e82608e7ad90",
+    "url": "/blueskies/static/js/2.a118853c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/blueskies/static/js/2.a118853c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb1b552c266845876c1a",
+    "url": "/blueskies/static/js/main.208b1857.chunk.js"
+  },
+  {
+    "revision": "f8e6784fce2305d939a6",
+    "url": "/blueskies/static/js/runtime-main.6e07d9b6.js"
+  },
+  {
+    "revision": "7c9793bf7c22fd284da0e704f8f9286a",
+    "url": "/blueskies/static/media/desert-landscape-desktop.7c9793bf.png"
+  },
+  {
+    "revision": "00ee27ed865f22d77c7cd0c1f4290ab9",
+    "url": "/blueskies/static/media/owfont-regular.00ee27ed.svg"
+  },
+  {
+    "revision": "3d73387c2a1e21f207a8357724f536ba",
+    "url": "/blueskies/static/media/owfont-regular.3d73387c.ttf"
+  },
+  {
+    "revision": "e00c269cd4fb37b24eea2e4f37982c3d",
+    "url": "/blueskies/static/media/owfont-regular.e00c269c.eot"
+  },
+  {
+    "revision": "ed668bf0b123371be46d005b428f6188",
+    "url": "/blueskies/static/media/owfont-regular.ed668bf0.woff"
+  }
+]);
