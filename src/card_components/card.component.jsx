e2f93256c17968391e2f176adf7ef9983241c9a2
ekaji <ekajionah@gmail.com>
2020-04-06 21:08:32 +0100

    seperated day string from date

--- src/card_components/card.component.jsx
+++ src/card_components/card.component.jsx
@@ -7,7 +7,8 @@ export const Card = (props) =>  {
     const date = moment(props.reading.dt_txt);
     return(
     <div className='card-container'>
-        <h3>{date.format('dddd-MMM-DD-YYYY')}</h3>
+        <h2>{date.format('dddd')}</h2>
+        <h3>{date.format('MMM-DD-YYYY')}</h3>
         <i className={`owf owf-${props.reading.weather[0].id} owf-5x`}></i>
         <h1>{Math.round((props.reading.main.temp)-273.15)}°c</h1>
         <h2>{props.reading.weather[0].description}</h2>
